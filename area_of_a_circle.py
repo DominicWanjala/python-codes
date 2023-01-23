c90f35346750384293a63645a3bccee52f5a4038
DominicWanjala <106527025+DominicWanjala@users.noreply.github.com>
2023-01-23 16:31:53 +0300

    Add files via upload

---
+++ area_of_a_circle.py
@@ -0,0 +1,7 @@
+# program to calculate the area of a circle.
+pie=3.142
+def area(radius):
+    return(area)
+radius = int(input("Enter the radius"))
+area = pie * (radius * radius)
+print("area =",area)
---
+++ gross_pay.py
@@ -0,0 +1,7 @@
+# program to calculate the gross pay.
+def gross_pay(hours_worked,rate_per_hour):
+    return(gross_pay)
+hours_worked = int(input("Enter the hours_worked "))
+rate_per_hour = float(input("Enter the rate per hour"))
+gross_pay = hours_worked * rate_per_hour
+print("agross_pay=",gross_pay)
